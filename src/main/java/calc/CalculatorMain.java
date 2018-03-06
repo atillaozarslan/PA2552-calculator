76da0a90b4179ea86b5d72482558d22bf031c1e4
Evanaellio <evan.mottais@gmail.com>
2018-03-06 16:27:08 +0100

    Add stubs for missing methods and enable caching in shippable.yml

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -1,10 +1,25 @@
 package calc;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class CalculatorMain {
-	public CalculatorMain(){
-		
-	}
-	public int add(int a, int b){
-		return a+b;
-	}
+    public CalculatorMain() {
+
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        throw new NotImplementedException();
+    }
+
+    public int mul(int a, int b) {
+        throw new NotImplementedException();
+    }
+
+    public int div(int a, int b) {
+        throw new NotImplementedException();
+    }
 }
